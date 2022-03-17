5dce93cf348fb58c5930db67d48c32cac66a4010
celeste drummond <parafactual@gmail.com>
2022-03-17 14:45:32 -0500

    random dictation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(never_type)]
+
 mod commands;
 mod data;
 mod discord;
@@ -6,14 +8,14 @@ mod prelude;
 
 use commands::handle_command;
 use discord::{reply, send, send_raw, set_own_nickname, GENERAL_ID, OWN_ID};
-use language::qa_prompt;
+use language::{dictum_prompt, qa_prompt};
 use prelude::*;
 
-use std::{fs, sync::Arc};
+use std::{fs, sync::Arc, time::Duration};
 
 use futures_util::StreamExt;
 use rand::{thread_rng, Rng};
-use tokio::sync::Mutex;
+use tokio::{sync::Mutex, time::sleep};
 use twilight_gateway::{Event, Intents, Shard};
 use twilight_http::Client as HttpClient;
 
@@ -54,6 +56,27 @@ async fn on_start(http: Arc<HttpClient>) {
     }
 }
 
+async fn random_event_loop_inner(http: Arc<HttpClient>) -> Res<!> {
+    // random dictation
+    loop {
+        if thread_rng().gen_bool(0.00005) {
+            let output = dictum_prompt().await?;
+            send(&http, GENERAL_ID, output.as_str()).await?;
+        }
+
+        sleep(Duration::from_millis(100)).await;
+    }
+}
+
+async fn random_event_loop(http: Arc<HttpClient>) {
+    loop {
+        if let Err(e) = random_event_loop_inner(http.clone()).await {
+            let message = format!("```\n{:?}\n```", e);
+            send_raw(&http, GENERAL_ID, message.as_str()).await.unwrap();
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Res<()> {
     let token = fs::read_to_string("token.txt")
@@ -71,6 +94,7 @@ async fn main() -> Res<()> {
     let conn = Arc::new(Mutex::new(redis.get_connection()?));
 
     tokio::spawn(on_start(http.clone()));
+    tokio::spawn(random_event_loop(http.clone()));
 
     let context = Context {
         http,
