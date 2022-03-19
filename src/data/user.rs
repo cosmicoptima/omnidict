7685432e96de80acded2bc1a08eeecbf5380f987
celeste drummond <parafactual@gmail.com>
2022-03-19 18:57:09 -0500

    Different DB keys to avoid conflict

--- src/data/user.rs
+++ src/data/user.rs
@@ -10,16 +10,16 @@ pub struct UserData {
 impl UserData {
     pub async fn load(id: Id<UserMarker>, db: Arc<Mutex<DbConnection>>) -> Result<UserData> {
         let mut db = db.lock().await;
-        let health: i32 = db.get(format!("users:{id}:health")).unwrap_or(100);
-        let gender: Option<String> = db.get(format!("users:{id}:gender"))?;
+        let health: i32 = db.get(format!("users>{id}>health")).unwrap_or(100);
+        let gender: Option<String> = db.get(format!("users>{id}>gender"))?;
         Ok(UserData { id, health, gender })
     }
 
     pub async fn save(&self, db: Arc<Mutex<DbConnection>>) -> Result<()> {
         let mut db = db.lock().await;
         let id = self.id;
-        db.set(format!("users:{id}:health"), &self.health)?;
-        db.set(format!("users:{id}:gender"), &self.gender)?;
+        db.set(format!("users>{id}>health"), &self.health)?;
+        db.set(format!("users>{id}>gender"), &self.gender)?;
         Ok(())
     }
 }
